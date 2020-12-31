4f5580dee2653f597062952c2826b6e746f4736f
Jesal Mehta <jesaldmehta@gmail.com>
2020-12-31 00:13:26 -0500

    Created seed.sql file to pre-populate the tables in the database

--- schema.sql
+++ schema.sql
@@ -1,6 +1,9 @@
 DROP DATABASE IF EXISTS gym_management_systemdb;
+
 CREATE DATABASE gym_management_systemdb;
 USE gym_management_systemdb;
+
+-- this table stores member's personal details
 CREATE TABLE member(
 	 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	 username VARCHAR(30) NOT NULL UNIQUE,
@@ -9,10 +12,11 @@ CREATE TABLE member(
 	 last_name VARCHAR(30)NOT NULL,
 	 gender VARCHAR(1),
 	 date_of_birth DATE,
-	 email VARCHAR(30),
+	 email VARCHAR(30) NOT NULL,
 	 phone BIGINT(10)
 );
 
+-- this table stores employee personal details including their roles
 CREATE TABLE employee(
 	 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	 username VARCHAR(30) NOT NULL UNIQUE,
@@ -20,12 +24,13 @@ CREATE TABLE employee(
 	 first_name VARCHAR(30) NOT NULL,
 	 last_name VARCHAR(30)NOT NULL,
 	 gender VARCHAR(1),
-	 email VARCHAR(30),
+	 email VARCHAR(30) NOT NULL,
 	 phone BIGINT(10),
      role VARCHAR(10) NOT NULL,
 	 manager_id INT
 );
 
+-- this table stores details of all the classes scheduled within the week by all the trainers
 CREATE TABLE class(
 	 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	 name VARCHAR(50) NOT NULL,
@@ -37,6 +42,7 @@ CREATE TABLE class(
 	 FOREIGN KEY (trainer_id) REFERENCES employee(id) ON DELETE CASCADE
 );
 
+-- this table stores details of all the members that have joined a class for a specific date
 CREATE TABLE class_members(
     class_id INT NOT NULL,
 	member_id INT NOT NULL,
