9566db873929c8cb56c0a876ef076eb2d7ef1575
Jesal Mehta <jesaldmehta@gmail.com>
2021-01-07 00:00:51 -0500

    Installed md5 npm package

--- Clients/clients.js
+++ Clients/clients.js
@@ -1,8 +1,14 @@
+// Dependencies 
+// md5 Copyright details: 
+// Copyright © 2011-2015, Paul Vorbach.
+// Copyright © 2009, Jeff Mott.
+var md5 = require('md5');
+
 // creates class Member with its own properties and methods
 class Member{
     constructor(username, password, first_name, last_name, gender, date_of_birth, email, phone){
         this.username = username;
-        this.password = MD5(password);
+        this.password = md5(password); // needs to be tested if it works when sign-up page is ready
         this.first_name = first_name;
         this.last_name = last_name;
         this.gender = gender;
