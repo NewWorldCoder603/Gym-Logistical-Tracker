d3c2cd74b399b1481b8e40f30bbac8cf55b9f1be
Sarah <70615195+smanter82@users.noreply.github.com>
2021-01-19 18:14:42 -0500

    polished register html and css

--- Public/assets/js/manager.js
+++ Public/assets/js/manager.js
@@ -1,41 +1,73 @@
-$("#submitRegistration").click(function () {
+//hire trainer variables
+const email = $("#inputEmail");
+const password = $("#inputPassword");
+const firstName = $("#firstName");
+const lastName = $("#lastName");
+const birthday = $("#inputBirthday");
+const gender = $("#inputGender");
+const phone = $("#inputPhone");
+const role = $("#inputRole");
+//trainer list variables
+const trainerList = $(".trainerNameList");
+
+//Get trainer list on page load.
+$("body").on("click", "#testBtn", function () {
   $.ajax({
-    url: `/api/member/${window.localStorage.getItem("userId")}`,
+    url: `/api/manager/trainers`,
     method: "GET",
-  }).then(function (data) {
-    console.log(data);
-    console.log("hello");
-    localStorage.clear();
-    window.location.replace("/");
+  }).then(function (trainerNames) {
+    // $(trainerList).empty();
+    console.log(trainerNames);
+    function loadTrainers() {
+      for (let i = 0; i < trainerNames.length; i++) {
+        console.log(trainerNames.first_name);
+        const firstName = trainerNames.first_name;
+        const lastName = trainerNames.last_name;
+        $(trainerList).append(`<ul style="list-style-type:none;">
+    <li class="me-5 float-left text-white">${firstName}${lastName} </li>
+    <button type="button" class="btn darkBtn d-flex float-right ms-5 mb-3">View</button>
+    </ul>  `);
+      }
+    }
+    loadTrainers();
   });
 });
 
-//   //Clear trails list at start of each search so that results don't stack. - SM
-//   $('#trailResults').empty();
+//submit button listener for hire new trainer
+$("body").on("click", "#hireBtn", function () {
+  $.ajax({
+    url: "/api/manager/addTrainer/",
+    data: {
+      email: email.val().trim(),
+      password: password.val().trim(),
+      first_name: firstName.val().trim(),
+      last_name: lastName.val().trim(),
+      date_of_birth: birthday.val().trim(),
+      gender: gender.val().trim(),
+      phone: phone.val().trim(),
+      role: role.val().trim(),
+    },
+    method: "POST",
 
-//   //Set trail information variables for ajax call and create for loop - SM
-//   for (let i = 0; i < response.trails.length; i++) {
-//     let trailName = response.trails[i].name;
-//     let trailLocation = response.trails[i].location;
-//     let trailSummary = response.trails[i].summary;
-//     let trailDifficulty = response.trails[i].difficulty;
+    error: function (req, status, err) {
+      if (err) alertModal(err);
+    },
+  }).then(function (response) {
+    console.log(response);
+    //If sign-up goes through, refresh manager page
+    window.location.href("/manager");
+  });
+});
 
-//     console.log(response.trails[i].name);
+//function to display Alert Modal on login error
+function alertModal(title, body) {
+  // Display error message to the user in a modal
+  $("#alert-modal-title").html(title);
+  $("#alert-modal-body").html(body);
+  $("#alert-modal").modal("show");
+}
 
-//     //Populate trail information list on page. -SM and TW
-//     $("#trailResults").append(`<div class="trailListResults">
-//     <div class="left-align">
-//     <a class=" trailButton waves-effect waves-light btn-small">${trailName}</a>
-//     </div>
-//     <div id="trailLocation" class="left-align trailData">
-//       <b>Location:</b> ${trailLocation}
-//     </div>
-
-//     <div id="trailSummary" class="left-align trailData">
-//       <b>Summary:</b> ${trailSummary}
-//     </div>
-
-//     <div id="trailRating" class="left-align trailData">
-//       <b>Difficulty:</b> ${trailDifficulty}
-//     </div>`);
-//   }
+//Close modal on close button click
+$(".modalBtn").click(function () {
+  $("#alert-modal").modal("hide");
+});
