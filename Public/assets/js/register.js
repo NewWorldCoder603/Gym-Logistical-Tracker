416eee580c6f12df704df34718dbb61153b078f3
Jesal Mehta <jesaldmehta@gmail.com>
2021-01-20 08:47:40 -0500

    Added validations to the registration fields and styled the error modal

--- Public/assets/js/register.js
+++ Public/assets/js/register.js
@@ -6,30 +6,39 @@ const birthday = $("#inputBirthday");
 const gender = $("#inputGender");
 const phone = $("#inputPhone");
 
-$("body").on("click", "#submitRegistration", function () {
-  $.ajax({
-    url: "/api/register/",
-    data: {
-      userName: email.val().trim(),
-      password: password.val().trim(),
-      first_name: firstName.val().trim(),
-      last_name: lastName.val().trim(),
-      date_of_birth: birthday.val().trim(),
-      gender: gender.val().trim(),
-      //do I still need email if it's already the username?
-      phone: phone.val().trim(),
-    },
-    method: "POST",
+$("body").on("click", "#submitRegistration", function(){
+  const formElem = document.getElementById("register_form");
+  // checks for form validation and sends an ajax call only when form fields are valid
+  const checkValid = formElem.checkValidity();
+  if(checkValid){
+    $.ajax({
+      url: "/api/register/",
+      data: {
+        userName: email.val().trim(),
+        password: password.val().trim(),
+        first_name: firstName.val().trim(),
+        last_name: lastName.val().trim(),
+        date_of_birth: birthday.val().trim(),
+        gender: gender.val().trim(),
+        phone: phone.val().trim(),
+      },
+      method: "POST",
 
-    error: function (req, status, err) {
-      if (err) alertModal(err);
-    },
-  }).then(function (response) {
-    console.log(response);
-    //If sign-up goes through, get request for specific member's id
-    localStorage.setItem("userId", response.id);
-    window.location.assign("/client-schedule");
-  });
+      error: function (req, status, err) {
+        console.log(req);
+        if (err) alertModal("Error", req.responseJSON.error);
+      },
+    }).then(function (response) {
+      console.log(response);
+      //If sign-up goes through, get request for specific member's id
+      localStorage.setItem("userId", response.id);
+      window.location.assign("/client-schedule");
+    });
+  } else{
+    // when form invalid, prevents submission and focuses into the 1st invalid field
+    document.querySelector('input:invalid').reportValidity();
+    document.querySelector('input:invalid').focus();
+  }
 });
 
 //function to display Alert Modal on login error
